fd9505b5a841aaeb2dd8c6db17764c1933bf59bc
Brandon <brandonkanute@gmail.com>
2022-04-05 10:10:36 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
-}
+    let input = document.getElementById('input');
+
+    function clickAlert(){
+        alert("This section has been clicked");
+    }
+
+    input.addEventListener('click',clickAlert);
+
+    
+};
+
+addingEventListener()
